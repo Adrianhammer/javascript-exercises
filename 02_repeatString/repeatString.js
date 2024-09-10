0423d3ae01cba14ae9d650e22124a8c68d3917f7
Adrianhammer <adrian10hammer@gmail.com>
2024-09-10 12:03:11 +0200

    2.oppgave

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  var word = "";
 
+  for (let count = 0; count < num; count++) {
+    word += string;
+  }
+
+  return word;
 };
 
 // Do not edit below this line
