e2fc356323c14f7724a76cac4da1409a4e548bc7
Adrianhammer <adrian10hammer@gmail.com>
2024-09-24 10:11:34 +0200

    yes

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,8 +1,12 @@
 const leapYears = function (year) {
   //Sjekk om det kan deles på 4
   //År som kan deles på 100 er ikke leap year med mindre de kan deles på 400
-
-  
+  const isCentury = year % 100 == 0;
+  if (year % 4 == 0 && (year % 400 == 0 || !isCentury)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degree) {
+  //Should return 0
+  //1 decimal number
+  //eg: 100fahreinheit == 37.8 celsius
+  //Formula F = (x-32) * 5/9
+  const celsius = ((degree - 32) * 5) / 9;
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degree) {
+  //Should return 32
+  //1 decimal number
+  //Formula C = (x * 9/5 + 32)
+  const fahrenheit = (degree * 9) / 5 + 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
